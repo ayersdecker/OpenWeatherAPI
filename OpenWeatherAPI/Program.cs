4df58df71e4693e59261f43d7071176fa65d991d
Decker Ayers <ayersdecker@gmail.com>
2023-03-01 12:22:56 -0500

    Initial Commit

---
+++ OpenWeatherAPI/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using Newtonsoft.Json;
+
+namespace OpenWeatherAPI
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+            if (MenuSelection()) 
+            {
+                await OpenWeather(44, -73.6);
+                //await InitiateCurrent();
+                
+            }
+            else { InitiateOther(); }
+
+
+        }
+        private static bool MenuSelection()
+        {
+            while (true)
+            {
+                Console.WriteLine("--- Weather App ---");
+                Console.WriteLine("\n1. Current Location\n2. Different City/State\n");
+                Console.Write("Selection: ");
+                if (int.TryParse(Console.ReadLine(), out int result) && result > 0 && result < 3) {
+                    if (result == 1) { return true; } else return false; }
+                else { Console.WriteLine("\n* Seleciton Error -- Try Again *\n"); }
+            }
+        }
+        private static async Task InitiateCurrent()
+        {
+            // Collect Host IP Address
+            
+
+            // Assemble Request
+            string url = "http://api.ipstack.com/check";
+            string key = "?access_key=a86738f3a1015863fb9485f079e9e159";
+            string requestCombo = url +  key;
+
+            // New Request 
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion");
+            HttpResponseMessage response = await client.GetAsync(requestCombo);
+            response.EnsureSuccessStatusCode();
+
+            //JSON Fun Time
+            string content = await response.Content.ReadAsStringAsync();
+            dynamic result = JsonConvert.DeserializeObject<dynamic>(content);
+            Console.WriteLine(requestCombo);
+            if(result == null) { Console.WriteLine("Null Result"); }
+            else 
+            { 
+                Console.WriteLine(result.latitude);
+                Console.WriteLine(result.longitude);
+                OpenWeather(result.latitude, result.longitude);
+            }
+            
+
+
+        }
+        private static void InitiateOther()
+        {
+
+        }
+        private static async Task OpenWeather(double lat, double lng)
+        {
+            
+            // Key and URL Build
+            string key = "3101f82226ed18424cb3d3077d5ffd37";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lng}&appid={key}";
+            
+
+            // Build and Connect to OpenWeather API
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion");
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            string content = await response.Content.ReadAsStringAsync();
+            dynamic result = JsonConvert.DeserializeObject<dynamic>(content);
+            Console.WriteLine(content);
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine($"\nSky:{result.weather}\n\nTemp: {result.main.temp} K");
+            Console.ResetColor();
+
+
+
+        }
+
+        
+    }
+}
---
+++ OpenWeatherAPI/WebScrapping.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using HtmlAgilityPack;
+using System.Net;
+
+namespace OpenWeatherAPI
+{
+        internal class WebScrapping
+    {
+        public async Task WebScrap()
+        {
+            Console.Write("What is your Zip?: ");
+            int.TryParse(Console.ReadLine(), out int zipOut);
+
+
+            string customURL = $"https://www.zipinfo.com/cgi-local/zipsrch.exe?ll=ll&zip={zipOut}&Go=Go";
+            //string customURL = $"https://www.google.com/maps/@{zipOut}";
+            //string customURL = $"https://forecast.weather.gov/zipcity.php?inputstring={zipOut}";
+            //string weatherUrl = "https://forecast.weather.gov/MapClick.php?lat=43.1&lon=-77.5#.Y9ADUcnMJy8";
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion");
+
+            HttpResponseMessage response = await client.GetAsync(customURL);
+
+            response.EnsureSuccessStatusCode();
+
+
+            //string returnedURL = client.BaseAddress.ToString();
+
+
+            /* int latStartIndex = returnedURL.IndexOf("@") + 1;
+            int latEndIndex = returnedURL.IndexOf(",");
+            string lat = returnedURL.Substring(latStartIndex, latEndIndex - latStartIndex);
+
+            int lonStartIndex = latEndIndex + 1;
+            int lonEndIndex = returnedURL.IndexOf("z");
+            string lon = returnedURL.Substring(lonStartIndex, lonEndIndex - lonStartIndex); */
+
+            string htmlF = await response.Content.ReadAsStringAsync();
+            HtmlDocument parserF = new HtmlDocument();
+            parserF.LoadHtml(htmlF);
+            HtmlNode table = parserF.DocumentNode.DescendantNodes(3).ElementAt(3);
+            HtmlNode tr2 = table.Descendants("tr").ElementAt(1);
+            IEnumerable<HtmlNode> tr = table.Descendants("tr");
+            double lat = Convert.ToDouble(tr2.Descendants("td").ElementAt(3).InnerText);
+            double lon = Convert.ToDouble(tr2.Descendants("td").ElementAt(4).InnerText);
+
+
+            string weatherURL = $"https://forecast.weather.gov/MapClick.php?lat={lat}&lon={lon}#.Y9KSz8nMLEY";
+
+            response = await client.GetAsync(weatherURL);
+
+            string html = await response.Content.ReadAsStringAsync();
+            HtmlDocument parser = new HtmlDocument();
+            parser.LoadHtml(html);
+
+            string tempF = parser.DocumentNode.DescendantNodes().First(node => node.HasClass("myforecast-current-lrg")).InnerText;
+            string tempC = parser.DocumentNode.DescendantNodes().First(node => node.HasClass("myforecast-current-sm")).InnerText;
+
+            tempF = WebUtility.HtmlDecode(tempF);
+            tempC = WebUtility.HtmlDecode(tempC);
+            Console.WriteLine($"{tempF}/{tempC}");
+        }
+    }
+   
+}
